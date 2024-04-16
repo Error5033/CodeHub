42649ce410af5c211d3867570f8e62202b6cb4f2
Your Name <k.zaksauskas@rgu.ac.uk>
2024-04-16 11:48:09 +0100

    security

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const app = express();
 const port = 3000;
 // MySQL Database Connection
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Zaksauskas123',
-    database: 'codeh'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 db.connect((err) => {
     if (err) {
